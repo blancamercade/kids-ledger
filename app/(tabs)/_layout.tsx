c00e4241dd81ba8300defb9a32909d1a38ae1b3d
blancamercade <145469413+blancamercade@users.noreply.github.com>
2025-04-12 16:40:50 +0800

    Update _layout.tsx

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,6 +11,7 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
+        headerShown: false,
         tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
         headerShown: true,
         headerTitle: selectedKid ? `${selectedKid}'s Account` : 'No Kid Selected',
